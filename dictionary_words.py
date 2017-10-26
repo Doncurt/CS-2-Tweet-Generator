ea23616b8feec4195842cf63994b7572c1174048
Donovan Adams <donovanadams@Donovans-MacBook-Pro.local>
2017-10-26 16:53:10 -0700

    finished dictionary words

---
+++ dictionary_words.py
@@ -0,0 +1,20 @@
+#imports the random class to use
+import random
+#function declaration for dictionaryWords
+def dictionaryWords(num):
+    #turns the command line input into an in that can be used for processing
+    num = int(num[0])
+    # opens up the file and sets it to a list thats split by its lines
+    lines = open('/usr/share/dict/words', 'rU').read().splitlines()
+    #using the command entered value as a sentinel it determines the amount of times it must print
+    for i in range (num):
+        #takes a random index in the lines list and prints
+        print (random.choice(lines))
+
+
+#set up for command line arguments
+if __name__ == '__main__':
+#imports the system module
+    import sys
+    #sets up the
+    dictionaryWords(sys.argv[1:])
--- rearrange.py
+++ rearrange.py
@@ -1,10 +1,16 @@
+#imports the shuffle from random
 from random import shuffle
 
 def rearrange(words):
+    #due to how commandline handles the arguments, it must be converted to a list for proocessing
     words = list(words)
+    #shuffle class is alled and the words are shuffled
     shuffle(words)
+    #the list is randomized then rejoined as a single string
     print (' '.join(words))
 
 if __name__ == "__main__":
+    #preps system for output
     import sys
+    #takes command line arguments
     rearrange(sys.argv[1:])
