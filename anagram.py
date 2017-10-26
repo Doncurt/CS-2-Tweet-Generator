58017c989419ca3940e05f7153a2172695065428
Donovan Adams <donovanadams@Donovans-MacBook-Pro.local>
2017-10-26 14:06:46 -0700

    final commit for anagram

--- anagram.py
+++ anagram.py
@@ -1,11 +1,12 @@
-import random
+#imports the shuffle module
 from random import shuffle
-import itertools
+
 def anagram(word):
-    #turns the word into a list of characters
-    s= list(str(word[0]))
-    shuffle(s)
-    print(''.join(s))
+    #turns the word into a list of characters the index is used becuase command line arguments are taken in a list format so the first word would be at index 0
+    shuffled = list(str(word[0]))
+    #shuffles words
+    shuffle(shuffled)
+    print(''.join(shuffled))
 if __name__ == "__main__":
     # imports system for commandline
     import sys
